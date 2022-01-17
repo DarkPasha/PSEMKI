8c35de4604ed9d54a8d6ad54a043939d892efd2f
HIZ <emirhanafsin.main@gmail.com>
2022-01-17 15:41:34 +0100

    V2 Code

--- carsanddoggs.py
+++ carsanddoggs.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 from PIL import Image
 from os import listdir
 
+import random
+
 
 normalize = transforms.Normalize(
     mean = [0.485, 0.456, 0.406]
@@ -19,15 +21,30 @@ transform = transforms.Compose([
 
 
 train_data_list=[]
-for f in listdir("catdog/train/"):
+traget_list = []
+train_data=[]
+
+
+files = listdir("catdog/train/")
+
+
+for i in range(len(listdir("catdog/train/"))):
+    f = random.choice(files)
+    files.remove(f)
     img = Image.open("catdog/train/" + f)
     img_tensor = transform(img)
     train_data_lost.append(img_tensor)
+    isCat = 1 if "cat" in f else 0
+    isDog = 1 if "dog" in f else 0
+    target = [isCat, isDog]
+    target_list.append(target)
 
-    if len(train_data_list) >= 256:
+
+    if len(train_data_list) >= 64:
+        train_data.append((torch.stack(train_data_list), target_list))
+        train_data_list = []
         break
-
-train_data = torch.stack(train_data_list)
 print(train_data)
+
 
 

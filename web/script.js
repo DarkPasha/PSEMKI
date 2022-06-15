2af3c80b1bc6f142395fbd5ee845de70eefbefc5
Joshi-Kartoshi <52852177+Joshi-Kartoshi@users.noreply.github.com>
2022-06-15 19:39:51 +0200

    web hinzugefügt
    html css und js ersetzt und hinzugefügt

--- web/script.js
+++ web/script.js
@@ -1,9 +1,10 @@
 const image_input = document.querySelector("#image-input");
+var uplouad_image = "";
 
 image_input.addEventListener("change", function() {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
+    uploaded_image = reader.result;
     document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
   });
   reader.readAsDataURL(this.files[0]);
